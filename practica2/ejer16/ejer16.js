28dfc81bdfc047c85581b7699aff13dced9f8882
Celina Garcia Carril <celigarcia1331@gmail.com>
2022-04-12 18:26:54 -0300

    .

---
+++ practica2/ejer16/ejer16.js
@@ -0,0 +1,9 @@
+function sumar(){
+    let res;
+
+    res = parseInt(document.getElementById("num1").value) + parseInt(document.getElementById("num2").value);
+    parseInt(res);
+
+    document.getElementById("resultado").value = res;
+
+}
