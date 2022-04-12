f396d66e459d09773ee57528622b129006c1de1f
Franco Garcia <francogarcia1331@gmail.com>
2022-04-12 16:29:12 -0300

    sigo sin saber implementar el reduce :(

--- practica2/ejer12/ejer12.js
+++ practica2/ejer12/ejer12.js
@@ -1,11 +1,12 @@
 var numbers = [ 1, 3, 4, 6, 23, 56, 56, 67, 3, 567, 98, 45, 480, 324, 546, 56 ];
 
 
-function reduce(array, binaryOperation, initialValue){  
-    array.forEach(element => {
-        binaryOperation(array[initialValue], array[initialValue+1]);
-    });
-
+function reduce(array = [], binaryOperation, initialValue){  
+    let res = 0;
+    for(let i = initialValue; i < array.length; i++){
+        res += binaryOperation(array[i], array[i+1]);
+    }
+    return res;
 }
 
 var sum = (x,y) => x + y;
