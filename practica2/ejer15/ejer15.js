e533aa2b45e25fa2e195447a7d4d46c3c81b601b
Franco Garcia <francogarcia1331@gmail.com>
2022-04-12 18:02:08 -0300

    ejer15 hechooo

--- practica2/ejer15/ejer15.js
+++ practica2/ejer15/ejer15.js
@@ -1,3 +1,11 @@
+//muestra en la consola el mensaje escrito en el input
 function enviar(){
     console.log(document.getElementById("texto").value);
+}
+
+//mueve el texto que se encuentra dentro del input texto2, hacia el input texto3
+function enviar2(){
+    let aux = document.getElementById("texto2").value;
+    document.getElementById("texto2").value = " ";
+    document.getElementById("texto3").value = aux;
 }
