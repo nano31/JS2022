fbe7de84fdbf2915f2199ef3dbd44af048753c0c
Franco Garcia <francogarcia1331@gmail.com>
2022-04-12 21:55:57 -0300

    .

--- practica2/ejer12/ejer12.js
+++ practica2/ejer12/ejer12.js
@@ -2,7 +2,9 @@ var numbers = [ 1, 3, 4, 6, 23, 56, 56, 67, 3, 567, 98, 45, 480, 324, 546, 56 ];
 var sum = 0;
 
 function reduce(array = [], binaryOperation, initialValue){  
-   binaryOperation(array,initialValue);
+   let acc = initialValue;
+   array.forEach((cur) => (acc, binaryOperation(acc,cur)));
+   return acc;
 }
 
 var sum = (x,y) => x + y;
@@ -11,7 +13,7 @@ console.log(numbers.reduce(sum,0));
 console.log(reduce(numbers,sum,0));
 
 
-
+ 
 //suma todos los elementos de un arreglo
 //var total = [0,1,2,3,4].reduce(function(a,b) {return a+b});
 
