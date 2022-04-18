c7bb3af07098864e208b3dcbb58210d2c1dd2bc3
Celina Garcia Carril <celigarcia1331@gmail.com>
2022-04-18 14:39:16 -0300

    inicio p3

---
+++ practica3/ejer2/ejer2.js
@@ -0,0 +1,16 @@
+var jsonStr = '['
++ '{"name":"Alice","dob": "2001-03-04T00:00:00.000Z","h": 165,"w": 68},'
++ '{"name":"Robert","dob": "1997-01-31T00:00:00.000Z","h": 170,"w": 88},'
++ '{"name":"Charles","dob": "1978-10-15T00:00:00.000Z","h": 188,"w":102},'
++ '{"name":"Lucía","dob": "1955-08-07T00:00:00.000Z","h": 155,"w": 61},'
++ '{"name":"Peter","dob": "1988-03-09T00:00:00.000Z","h": 165,"w": 99},'
++ '{"name":"Lucas","dob": "1910-12-04T00:00:00.000Z","h": 172,"w": 75}]';
+
+function overweigthNames(people = []){
+    return people
+    .filter(p => (p.w / Math.pow(p.h / 100, 2)) > 25)
+    .map(p => p.name)
+    .reduce((n1, n2) => n1 + ", " + n2)
+}
+
+JSON.overweigthNames(jsonStr);
