75570d1045b2d3e77fdef4dd2d84b033e6e28a99
Franco Garcia <francogarcia1331@gmail.com>
2022-04-26 18:42:30 -0300

    ejer5

--- practica3/ejer5/ejer5.js
+++ practica3/ejer5/ejer5.js
@@ -1,6 +1,5 @@
 const API_URL = 'https://rickandmortyapi.com/api/episode';
 const container = document.getElementById("episodes-container")
-const searchBtn = document.getElementById("search-button")
 
 
 function fetchEpisode(){
@@ -11,6 +10,22 @@ function fetchEpisode(){
             ShowEpisode(elem);
         })
     })
+}
+
+function searchEpisode(){
+    container.innerHTML = ''; //limpia la pantalla
+    const searchValue = document.getElementById("search-episode").value;
+
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => {
+        let episode = data.results[0].name;
+        const li = document.createElement('li');
+        li.innerHTML = episode;
+        container.appendChild(li);
+    })
+    .catch(err => alert(err))
+
 }
 
 function ShowEpisode(episode){
