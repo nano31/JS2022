fd5d0d944c1b9d38ed0fdde56cc4ca6e4b4154c2
Franco Garcia <francogarcia1331@gmail.com>
2022-04-26 18:05:04 -0300

    ejer 4 hecho

--- practica3/ejer4/ejer4.js
+++ practica3/ejer4/ejer4.js
@@ -2,38 +2,37 @@
 const container = document.getElementById("episodes-container");
 
 function RickAndMorty(id){
-    fetch(`https://rickandmortyapi.com/api/episode/${id}/`)
+    fetch(`https://rickandmortyapi.com/api/episode/`)
     .then(response => response.json())
+    
     .then(data =>{
-        ShowEpisode(data)
-        //console.log(data)
+        //ShowEpisode(data)
+        data.results.map(elem =>{
+            ShowEpisode(elem)
+        })
     })
 }
 
-function getEpisode(number){
-    for(let i = 1; i <= number; i++){
-        RickAndMorty(i);
-    }
-}
+// function getEpisode(number){
+//     for(let i = 1; i <= number; i++){
+//         RickAndMorty(i);
+//     }
+// }
 
-getEpisode(8);
+// getEpisode(20);
 
 function ShowEpisode(episode){
     const li = document.createElement('li');
     li.classList.add("episode");
 
     const name = document.createElement("p");
-    name.textContent = episode.name
-
-    const episodeUrl = document.createElement("p");
-    episodeUrl.textContent = episode.url;
+    name.textContent = episode.name;
 
     const airDate = document.createElement("p");
-    airDate.textContent = episode.air_date;
+    airDate.textContent = episode.created;
 
     li.appendChild(name);
     li.appendChild(airDate);
-    li.appendChild(episodeUrl);
 
     container.appendChild(li);
 
