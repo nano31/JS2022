c352c6bc96bde34e27aa6744363a0b7ad3895c6f
Franco Garcia <francogarcia1331@gmail.com>
2022-04-26 15:49:34 -0300

    error en pokeApi

---
+++ pruebaPokeApi/main.js
@@ -0,0 +1,41 @@
+const pokemonContainer = document.querySelector('.pokemon-container');
+
+function fetchPokemon(id){ //se puede pasar el nombre o el if
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(res => res.json()) //convierte lo obtenido a un objeto json
+    .then(data =>{
+        createPokemon(data);
+    }) //muestra en la consola 
+}
+
+function fetchPokemons(number){
+    //nos permite obtener la cantidad de pokemons deseada, el valor se pasa por parámetro
+    //el indice arranca desde el 1, ya que así nos permite traer desde el pokemon 1, que en 
+    //este caso es Bulbasaur.
+    for(let i = 1; i <= number; i++){
+        fetchPokemon(i);
+    }
+}
+
+
+function createPokemon(pokemon){
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("card-container");
+
+    const card = document.createElement("div");
+    card.classList.add("pokemon-block");
+
+    const name = document.createElement("p");
+    name.textContent = pokemon.name;
+
+    const number = document.createElement("p");
+    number.textContent = `#${pokemon.id}`;
+
+    card.appendChild(name);
+    card.appendChild(number);
+
+    cardContainer.appendChild(card);
+    pokemonContainer.appendChild(cardContainer);
+}
+
+fetchPokemons(9); //muestra los primeros 9 pokemons en la consola
